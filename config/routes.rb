4e6f991eef963af7c745429f958b585110311db6
figs <figs.jordi@gmail.com>
2017-01-16 23:23:41 -0600

    with modals

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "/home" => "pages#show", page: "home", as: 'home'
   get "/about" => "pages#show", page: "about", as: 'about'
   get "/freelancer" => "pages#show", page: "freelancer", as: 'freelancer'
-  get "/blog" => "pages#show", page: "blog"
+  #get "/blog" => "pages#show", page: "blog"
+  get "/portfolio" => "pages#show", page: "portfolio"
   root "pages#show", page: "about"
 end
