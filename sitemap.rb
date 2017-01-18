039478a454353dfdf95521a21de8becb7a80e426
figs <figs.jordi@gmail.com>
2017-01-18 14:37:17 -0600

    sitemap 2

--- sitemap.rb
+++ sitemap.rb
@@ -4,6 +4,6 @@ require 'sitemap_generator'
 SitemapGenerator::Sitemap.default_host = 'http://www.poweredbyfigs.com'
 SitemapGenerator::Sitemap.create do
   add '/about', :changefreq => 'daily', :priority => 0.9
-  add '/portfolio', :changefreq => 'weekly'
+  add '/portfolio ', :changefreq => 'weekly'
 end
 SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
